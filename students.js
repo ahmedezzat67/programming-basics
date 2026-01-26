7015b7ea778eaa6d8b527ee5a91a5209e90eca17
ahmedezzat67 <134937371+ahmedezzat67@users.noreply.github.com>
2026-01-26 22:34:38 +0200

    Add files via upload

--- students.js
+++ students.js
@@ -1,5 +1,7 @@
 const students = [
   { email: "student1@gmail.com", code: "A1234" },
   { email: "student2@gmail.com", code: "B5678" },
-  { email: "student3@gmail.com", code: "C9012" },
+
+  // الطالب الجديد
+  { email: "newstudent@gmail.com", code: "X7kP9Z2Q1M" },
 ];
